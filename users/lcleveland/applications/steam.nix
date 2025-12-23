902bd6ca7287e94187e4d176621abe17941c4e4b
Lyle Cleveland <lyle.cleveland@proton.me>
2025-12-23 17:16:10 -0600

    remove unused configuration files for steam, ollama, and open-webui

--- users/lcleveland/applications/steam.nix
+++
@@ -1,6 +0,0 @@
-{ config, lib, ... }:
-{
-  config.programs.steam = {
-    enable = true;
-  };
-}
--- users/lcleveland/keyboard_layout.nix
+++ users/lcleveland/keyboard_layout.nix
@@ -1,4 +1,10 @@
 { config, lib, ... }:
 {
-
+  config = {
+    eiros.system.hardware.keyboard.variant = "colemak_dh";
+    eiros.system.desktop_environment.dank_material_shell.hyprland.input = {
+      kb_layout = "us";
+      kb_variant = "colemak_dh";
+    };
+  };
 }
