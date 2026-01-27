f24b01f8721c2cea1e010986721e3883400e8060
Lyle Cleveland <lyle.cleveland@proton.me>
2026-01-27 10:42:48 -0600

    test fix

--- users/lcleveland/mangowc.nix
+++ users/lcleveland/mangowc.nix
@@ -7,7 +7,9 @@
     edge_scroller_pointer_focus = 0;
 
     tagrule = [
-      "tag:*,layout:scroller"
+      "id:0,monitor_name:eDP-1,layout_name:scroller"
+      "id:0,monitor_name:DP-10,layout_name:scroller"
+      "id:0,monitor_name:DP-11,layout_name:scroller"
     ];
 
     env = [
