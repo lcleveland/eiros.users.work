4b80070181330f6d343ff15835284b3e6b9e667a
Lyle Cleveland <lyle.cleveland@proton.me>
2026-03-24 13:19:31 -0500

    add printing support

---
+++ users/lcleveland/printing.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  config = {
+    services = {
+      printing = {
+        enable = true;
+        drivers = [
+          pkgs.hplip
+        ];
+      };
+      avahi = {
+        enable = true;
+        nssmdns4 = true;
+        openFirewall = true;
+      };
+      system-config-printer = {
+        enable = true;
+      };
+    };
+  };
+}
