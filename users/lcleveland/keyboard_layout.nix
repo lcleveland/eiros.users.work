e16b0e39120a4412ab8d809187acdb04178c8e64
Lyle Cleveland <lyle.cleveland@proton.me>
2026-04-07 08:01:53 -0500

    remove hyprland settings

--- users/lcleveland/keyboard_layout.nix
+++ users/lcleveland/keyboard_layout.nix
@@ -2,10 +2,6 @@
 {
   config = {
     eiros.system.hardware.keyboard.variant = "colemak_dh";
-    eiros.system.desktop_environment.dank_material_shell.greeter.hyprland.sections.input = {
-      kb_layout = "us";
-      kb_variant = "colemak_dh";
-    };
     eiros.users.lcleveland.mangowc.settings = {
       xkb_rules_layout = "us";
       xkb_rules_variant = "colemak_dh";
