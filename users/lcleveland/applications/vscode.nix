61284b2a9e313e74c79d3ada36573d1c01c72552
Lyle Cleveland <lyle.cleveland@proton.me>
2026-04-14 12:25:17 -0500

    temp fix hash

--- users/lcleveland/applications/vscode.nix
+++ users/lcleveland/applications/vscode.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   config.programs.vscode = {
     enable = true;
@@ -7,14 +7,23 @@
         wrapProgram $out/bin/code --add-flags "--password-store=gnome-libsecret"
       '';
     });
-    extensions = with pkgs.vscode-extensions; [
-      continue.continue
-      jnoortheen.nix-ide
-      vscodevim.vim
-      github.copilot-chat
-      platformio.platformio-vscode-ide
-      ms-vscode.cpptools-extension-pack
-      ms-vscode.cpptools
-    ];
+    extensions =
+      (with pkgs.vscode-extensions; [
+        continue.continue
+        jnoortheen.nix-ide
+        vscodevim.vim
+        github.copilot-chat
+        platformio.platformio-vscode-ide
+        ms-vscode.cpptools-extension-pack
+        ms-vscode.cpptools
+      ])
+      ++ [
+        (pkgs.vscode-extensions.anthropic.claude-code.override {
+          src = pkgs.fetchurl {
+            url = "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/anthropic/vsextensions/claude-code/latest/vspackage";
+            sha256 = "0djanpdnmy0n02l8id5zpw8z2phpjv8ybdmccr5vzl938kqgvj6k";
+          };
+        })
+      ];
   };
 }
