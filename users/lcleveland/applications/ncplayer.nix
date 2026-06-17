73867c9ff9eec0c39073163fdf63f5c68626b003
Lyle Cleveland <lyle.cleveland@proton.me>
2026-06-17 07:55:14 -0500

    add ncplayer

---
+++ users/lcleveland/applications/ncplayer.nix
@@ -0,0 +1,62 @@
+{ pkgs, ... }:
+let
+  version = "13.35.8340";
+
+  src = pkgs.fetchurl {
+    url = "https://resources.ninjarmm.com/development/ninjacontrol/${version}/ninjarmm-ncplayer-${version}_x86_64.rpm";
+    hash = "sha256-QMvP/rGmQwHZiBFMnnz60RrIERWgu7HE4F+eHstnoPw=";
+  };
+
+  # Extract the single self-contained binary from the RPM.
+  ncplayer-bin = pkgs.stdenv.mkDerivation {
+    pname = "ninjarmm-ncplayer-bin";
+    inherit version src;
+    nativeBuildInputs = [ pkgs.rpmextract ];
+    unpackPhase = "rpmextract ${src}";
+    installPhase = ''
+      install -Dm755 opt/ncplayer/bin/ncplayer $out/bin/ncplayer
+    '';
+  };
+
+  # Wrap in an FHS environment to satisfy the binary's runtime library deps.
+  ncplayer = pkgs.buildFHSEnv {
+    name = "ncplayer";
+    targetPkgs =
+      pkgs: with pkgs; [
+        libdrm
+        libgbm
+        mesa
+        dbus
+        stdenv.cc.cc.lib
+      ];
+    runScript = pkgs.writeShellScript "ncplayer-run" ''
+      export QT_QPA_PLATFORM=xcb
+      exec ${ncplayer-bin}/bin/ncplayer "$@"
+    '';
+  };
+
+  # Desktop entry registering the ninjarmm:// URL scheme so the portal can
+  # launch remote sessions. Exec=ncplayer resolves to the FHS wrapper on PATH.
+  ncplayer-desktop = pkgs.writeTextFile {
+    name = "ninjarmm-ncplayer-desktop";
+    destination = "/share/applications/ninjarmm-ncplayer.desktop";
+    text = ''
+      [Desktop Entry]
+      Type=Application
+      Name=NinjaOne Remote Player
+      Exec=ncplayer %u
+      StartupNotify=false
+      MimeType=x-scheme-handler/ninjarmm;
+    '';
+  };
+in
+{
+  config = {
+    environment.systemPackages = [
+      ncplayer
+      ncplayer-desktop
+    ];
+
+    xdg.mime.defaultApplications."x-scheme-handler/ninjarmm" = "ninjarmm-ncplayer.desktop";
+  };
+}
